057ee7998dbdae785e81d8b640f062f65c24b4a6
Mathys Fornasier <mathysfornasier@MacBook-Pro-de-Mathys.local>
2022-08-01 17:08:02 +0200

    first commit MAP

--- frontSpeedShopping/screens/map.js
+++ frontSpeedShopping/screens/map.js
@@ -1,16 +1,13 @@
 import React, {useState, useEffect} from 'react' ;
-import { View,Text} from 'react-native' ;
+import { View} from 'react-native' ;
 import {Button,Overlay, Input,ListItem} from 'react-native-elements';
-import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
-
+import MapView, {Marker} from 'react-native-maps';
+import {useSelector, useDispatch} from 'react-redux';
 import * as Location from 'expo-location';
 import { FontAwesome } from '@expo/vector-icons'; 
-import  MapViewDirections from'react-native-maps-directions'
-import {store} from './reducers/redux' ;
-
 
 // import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import socketIOClient from "socket.io-client";
 
 // var socket = socketIOClient("http://172.20.10.2:3000");
 
@@ -18,54 +15,25 @@ import {store} from './reducers/redux' ;
 // pour itineraire https://www.npmjs.com/package/react-native-maps-directions
 
 
-export default function App() {
-
-
+function MapScreen(props) {
 
 
 // ***************************** ETATS ****************************** 
 
         const [currentLatitude, setCurrentLatitude] = useState(0) ; 
         const [currentLongitude, setCurrentLongitude] = useState(0) ;
-        // const [commercantList, setCommercantList] = useState([]) ;
+        const [listMarker, setListMarker] = useState([]) ;
 
 
-     
+        const [latitude , setLatitude] = useState(0);
+        const [longitude , setLongitude] = useState(0);
+        const [addPOI, setAddPOI] = useState(false) ;
         const [isOpen, setIsOpen] = useState(false) ;
+        const [listPOI, setListPOI] = useState([]) ;
+        const [textTitle , setTextTitle] = useState('') ;
         const [textDesc , setTextDesc] = useState('') ;
-        const [nomMagasin,setNomMagasin] = useState('');
-     
-
-
-  const commercantList = [{
-    nom:'Macdo',
-    horaire: '10h-17h',
-    latitude:  48.9,  
-    longitude: 2.333333
-  },{
-    nom:'burgerKing',
-    horaire: '10h-17h',
-    latitude:  48.9,  
-    longitude: 2.34
-  },{
-    nom:'lidl',
-    horaire: '10h-17h',
-    latitude:  48.866667,  
-    longitude: 2.355555,
-    coordinate: '48.866667, 2.355555'
-  },{
-    nom:'sushi',
-    horaire: '10h-17h',
-    latitude:  48.866667,  
-    longitude: 2.37
-  },{
-    nom:'carotte',
-    horaire: '10h-17h',
-    latitude:  48.866667,  
-    longitude: 2.32
-  },
-] ;
-
+        const dispatch = useDispatch() ;
+        const test = useSelector((state)=> state ) ;
 
 
 // ***************************** ASK & SET localistaion ****************************** 
@@ -77,42 +45,32 @@ export default function App() {
             setErrorMsg('Permission to access location was denied');
             return;
           }
-          Location.watchPositionAsync({distanceInterval: 10}, (location) => { setCurrentLatitude(location.coords.latitude), setCurrentLongitude(location.coords.longitude) 
-
-        // var data = await fetch("http://192.168.0.109:3000/map") ;
-          // var response = await data.json();
-
-          // // setCommercantList(response.commercant);
-          // // setCurrentLatitude(response.adresse);
-          // // setCurrentLongitude(response.adresse); 
-         });
+         Location.watchPositionAsync({distanceInterval: 10}, (location) => { setCurrentLatitude(location.coords.latitude), setCurrentLongitude(location.coords.longitude) 
+          socket.emit("sendPosition",location.coords.latitude , location.coords.longitude) ; 
+        });
         
       })();
-    }) ;
 
 
-      
+    AsyncStorage.getItem("POI", function(error, data) {
+      var dataParse = JSON.parse(data) ;
+     if (dataParse !== null) {
+      dataParse.map((poi,i)=>{
+        dispatch({
+          type:'POI/setPOI',
+          payload: {
+            title:poi.title,
+            description:poi.description,
+            latitude:poi.latitude,
+            longitude:poi.longitude
+          }
+         }) 
+      })
+     }
 
+    });
 
-    // AsyncStorage.getItem("POI", function(error, data) {
-    //   var dataParse = JSON.parse(data) ;
-    //  if (dataParse !== null) {
-    //   dataParse.map((poi,i)=>{
-    //     dispatch({
-    //       type:'POI/setPOI',
-    //       payload: {                           STORE
-    //         title:poi.title,
-    //         description:poi.description,
-    //         latitude:poi.latitude,
-    //         longitude:poi.longitude
-    //       }
-    //      }) 
-    //   })
-    //  }
-
-    // });
-
-
+    }, []) ;
 
 
 
@@ -125,11 +83,12 @@ export default function App() {
 
 // ***************************** ADD Lat/Lng to State ****************************** 
 
-   var test = (bool,nom) => {
-
-    setIsOpen(bool) ;
-    console.log(nom);
-        
+   var selectPOI = (e) => {
+        if (addPOI) {
+                setLatitude(e.nativeEvent.coordinate.latitude)
+                setLongitude(e.nativeEvent.coordinate.longitude)
+        setIsOpen(true) ;
+        }
 }
 // ***************************** ADD commercant to itinéraire ****************************** 
 
@@ -141,9 +100,8 @@ function testAddToItinéraire (title , description,) {
 
 // ***************************** SHOW ALL THE MARKER  ****************************** 
         
- var markerList =  commercantList.map((commercant,i)=>{
-
-return <Marker key={i} pinColor='blue' onPress={()=>test(true,commercant.nom)} title={commercant.nom} description={commercant.horaire} coordinate={{latitude: commercant.latitude, longitude: commercant.longitude}}/>
+ var markerList =  test.savePOI.map((POI,i)=>{
+        return <Marker key={i} pinColor='blue' title={POI.title} description={POI.description} coordinate={{latitude: POI.latitude, longitude: POI.longitude}}/>
  })        
 
 
@@ -151,23 +109,22 @@ return <Marker key={i} pinColor='blue' onPress={()=>test(true,commercant.nom)} t
  
 return(
 
-
-
  <View style= {{flex: 1}}>
 
 {/* ***************************** OVERLAY   ******************************  */}
-<View style={{height:400}}>
-
 
 
 <Overlay isVisible={isOpen} overlayStyle={{width: '70%'}} onBackdropPress={closeOverlay}>
-              <Text>{} </Text>
+                <Input
+                inputStyle= {{flex:1}}
+                placeholder='Titre'
+                onChangeText={(value) => setTextTitle(value)} 
+                value={textTitle} />
         
                 <Input
                    placeholder='Description '
                    onChangeText={(value) => setTextDesc(value)} 
-                   value={textDesc} 
-                   />
+                   value={textDesc} />
                 
                 <Button
                 icon={
@@ -177,7 +134,7 @@ return(
                     color="#ffffff"
                  />}
                 buttonStyle={{backgroundColor:'#eb4d4b'}}
-                title="Ajouter a l'itinéraire"
+                title="Add POI"
                 type="solid"
                 // onPress={() => testAddToItinéraire(textTitle, textDesc)}
                 />
@@ -186,25 +143,15 @@ return(
 {/* ***************************** MAP INITIAL SETTING  ******************************  */}
 
 <MapView style={{flex : 1}}
-  provider = {PROVIDER_GOOGLE}
-  initialRegion={{
+onPress={(e) =>{selectPOI(e)}}
+  showsTraffic={true}
+ initialRegion={{
    latitude:  48.866667,  // pour centrer la carte, REUSSIER A LE FAIRE SUR POS UTILISATEUR
    longitude: 2.333333,
    latitudeDelta: 0.0922,  // le rayon à afficher à partir du centre
    longitudeDelta: 0.0421,
 }} >
 
-<MapViewDirections
-    origin={commercantList[3]}
-    // waypoints={commercantList[2]}
-    destination={commercantList[4]}
-    mode="WALKING"
-    optimizeWaypoints={true}
-    onReady={(result)=> {console.log(result);}}
-    timePrecision="now"
-    apikey='AIzaSyD5OG3mJyZ7ogU9wiuUmngHz2GOvBr9SqU'
-    strokeWidth={3}
-  />
 
 
 
@@ -215,33 +162,30 @@ return(
 <Marker coordinate={{
         latitude: currentLatitude , longitude: currentLongitude
       }}
-
      title="Hello"
      description="I am here"
      draggable  // Rendre le marqueur drag & dropable
     />
 {/* ***************************** MARQERUR LISTE  ******************************  */}
 
-  {markerList} 
+ {markerList} 
 
 </MapView> 
-</View>
-
 <Text>Adaptez votre itinéraire en fonction de vos envies </Text>
 
 
 {/* *****************************  LISTE COMMERCANT CHOISI  ******************************  */}
 
 {
-    // listCommercant.map((l, i) => (
-    //   <ListItem key={i} bottomDivider>
-    //    <Avatar source={{uri: l.avatar_url}} />  {/* photo du commercant */}
-    //     <ListItem.Content>
-    //       <ListItem.Title>{l.name}</ListItem.Title> {/* nom du commercant */}
-    //       <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
-    //     </ListItem.Content>
-    //   </ListItem>
-    // ))
+    listCommercant.map((l, i) => (
+      <ListItem key={i} bottomDivider>
+       <Avatar source={{uri: l.avatar_url}} />  {/* photo du commercant */}
+        <ListItem.Content>
+          <ListItem.Title>{l.name}</ListItem.Title> {/* nom du commercant */}
+          <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    ))
   }
 
  </View>
@@ -255,3 +199,5 @@ return(
 
 
 
+
+export default MapScreen ;
