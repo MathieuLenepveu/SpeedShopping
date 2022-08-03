89895dd670903937d4931644b4497280bf90f770
geoffroy goirand <geoffroy.goirand@gmail.com>
2022-08-03 10:54:13 +0200

    commercant2

--- frontSpeedShopping/screens/commandesCommercant.js
+++ frontSpeedShopping/screens/commandesCommercant.js
@@ -12,13 +12,17 @@ import {Button,Input } from 'react-native-elements';
 
 export default function commandesCommercant() {
     return (
-      <View>
-<Text>
-PAGE DE SUIVI DE COMMANDE COMMERCANT
-</Text>
-
-
-</View>
+      <View style={styles.container}>
+      <FlatList
+        data={[
+          {key: 'Vin rouge', price: '20€'},
+          {key: 'Vin blanc', price: '20€'},
+          {key: 'Vin rosé', price: '20€'},
+          {key: 'Saucisson', price: '20€'}, 
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
+    </View>
 
     ) 
   }
