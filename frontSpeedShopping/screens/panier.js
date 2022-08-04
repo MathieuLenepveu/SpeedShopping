1492cba46a7252140a929593d6ce35364050709f
geoffroy goirand <geoffroy.goirand@gmail.com>
2022-08-04 14:36:19 +0200

    commercant53

--- frontSpeedShopping/screens/panier.js
+++ frontSpeedShopping/screens/panier.js
@@ -1,28 +1,50 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
 import {Button,Input } from 'react-native-elements';
-
-
-
-
-
 
 
 
 
 export default function panier(props) {
     return (
-      <View>
+      <View style={styles.container} >
 <Text>
 PANIER CLIENT
 </Text>
 
-
-<Button title="PASSER COMMANDE"
-        onPress={() => props.navigation.navigate('Paiement')}
-      />
+  
+<Pressable style={styles.button} 
+onPress={() => props.navigation.navigate('Paiement')}>
+<Text style={styles.text}>Payer</Text>
+        
+        </Pressable>
 
 </View>
 
     ) 
   }
+
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center", 
+    },
+    text: {
+      fontSize: 16, 
+      color: 'white', 
+    },
+    button: {
+      padding: 20,
+      margin: 10,
+      alignItems: "center", 
+      justifyContent: "center", 
+      borderWidth: StyleSheet.hairlineWidth,
+      borderColor: '#f0f0f0',
+      backgroundColor: '#052640', 
+      width: 200, 
+      borderRadius: 8, 
+      color: "#FFFFFF"
+    }
+  });
